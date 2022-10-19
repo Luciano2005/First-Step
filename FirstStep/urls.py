a06d0e5428833864b065a5e1aeaa1d258ec7dbb5
Luciano <@compu>
2022-10-19 12:47:19 -0500

    Solucion de error en enlaces entre aplicaciones

--- FirstStep/urls.py
+++ FirstStep/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('MAIN.urls')),
-    path('estudio/', include('ESTUDIO.urls'))
+    path('estudio/', include(('ESTUDIO.urls', 'estudio')))
 ]
