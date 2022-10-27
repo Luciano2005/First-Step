1106447bec1ff72d788762b3ae3da09c57bd64e7
Luciano2005 <lumanriquem@unal.edu.co>
2022-10-26 20:24:00 -0500

    Clases en los campos de cada form

--- ESTUDIO/forms.py
+++ ESTUDIO/forms.py
@@ -1,17 +1,19 @@
 from django import forms
-from django.forms import ModelForm
+from django.forms import ModelForm, Widget
 from ESTUDIO.models import Pregunta, RespuestasCerradas
 from django.forms.widgets import HiddenInput
 
 class newPregunta(forms.ModelForm):
     class Meta:
         model=Pregunta
-        fields=['name','respuesta','apropiacion','ultima_vez','seccion']
         widgets = {
             'apropiacion' : HiddenInput(),
             'ultima_vez' : HiddenInput(),
-            'seccion' : HiddenInput()
+            'seccion' : HiddenInput(),
+            'name' : forms.TextInput(attrs=({'class':'titulo'}))
         } 
+        fields=['name','respuesta','apropiacion','ultima_vez','seccion']
+        
 
 class newPreguntaCerrada(forms.ModelForm):
     class Meta:
