ace3e2b4f1030ea695355447a6fd148a18bcba03
Alejandro Diaz Cordoba <aldico2004@hotmail.com>
2022-10-28 12:10:46 -0500

    "Crispy implementado, formulario creación materias, uptade a nav materias"

--- FirstStep/settings.py
+++ FirstStep/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'MAIN',
     'ESTUDIO',
     'COMUNIDAD',
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -69,6 +71,10 @@ TEMPLATES = [
         },
     },
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 WSGI_APPLICATION = 'FirstStep.wsgi.application'
 
--- MAIN/forms.py
+++ MAIN/forms.py
@@ -1,7 +1,9 @@
+from time import time
 from django import forms
+from django.forms import ModelForm, Widget
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-
+from django.forms.widgets import HiddenInput
 from MAIN.models import Materia, Seccion
 
 class Registro(UserCreationForm):
@@ -36,6 +38,12 @@ class newMateria(forms.ModelForm):
     class Meta:
         model=Materia
         fields=['name','hora','profesor','profesor_email']
+        widgets = {
+            'name' : forms.TextInput(attrs=({'placeholder':'Nombre de la materia', 'class':'form-control'})),
+            'profesor' : forms.TextInput(attrs=({'placeholder':'Nombre del profesor', 'class':'form-control'})),
+            'profesor_email' : forms.TextInput(attrs=({'placeholder':'name@unal.edu.co', 'class':'form-control'})),
+            'hora' : forms.TimeInput(attrs=({'type':'time'})),
+        } 
 
 class newSeccion(forms.ModelForm):
     class Meta:
