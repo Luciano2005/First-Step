ec530ce28a4a2abb07d0b7e9792d649c2e3b2d3b
Luciano2005 <lumanriquem@unal.edu.co>
2022-11-05 12:41:04 -0500

    Intento 3

--- FirstStep/settings.py
+++ FirstStep/settings.py
@@ -157,9 +157,9 @@ MEDIA_URL ='/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
-STATICFILES_DIRS=(
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS=(
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 LOGIN_URL = '/formlogin'
 
