6f60e26bfef21f670f2482ba83713c4892b853fa
Luciano2005 <lumanriquem@unal.edu.co>
2022-11-05 16:07:03 -0500

    Modo prueba desactivado

--- FirstStep/settings.py
+++ FirstStep/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
