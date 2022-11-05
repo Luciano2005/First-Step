cc8553400b850537ad6c334b0b35128d8c8a15a1
Luciano2005 <lumanriquem@unal.edu.co>
2022-11-05 14:28:12 -0500

    Intento 6 | Debug=True, Renombre de catpetas Templates por templates

--- FirstStep/settings.py
+++ FirstStep/settings.py
@@ -146,6 +146,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+#--------------------------Recordar descomentar--------------------------------
 #if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
