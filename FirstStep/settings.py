f6fb0b42c043286dc614676854077f23b3ba9c98
Luciano2005 <lumanriquem@unal.edu.co>
2022-11-05 15:58:57 -0500

    Main se convierte en la rama de lanzamiento

--- FirstStep/settings.py
+++ FirstStep/settings.py
@@ -147,12 +147,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 #--------------------------Recordar descomentar--------------------------------
-#if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
+if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# Turn on WhiteNoise storage backend that takes care of compressing static files
-# and creating unique names for each version so they can safely be cached forever.
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL ='/media/'
 
