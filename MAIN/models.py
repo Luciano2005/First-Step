ec015de148e5b4053d7e1f1da0949fd8c8677546
Nicolas <nhinestrozag@unal.edu.co>
2022-11-18 11:00:27 -0500

    Horarioo

--- MAIN/models.py
+++ MAIN/models.py
@@ -22,7 +22,7 @@ class Materia(models.Model):
         ("Sábado", "Sábado"),
         ("Domingo", "Domingo"),
     )
-    horario = MultiSelectField(choices=OPTIONS, max_length=100, null=True)
+    horario = MultiSelectField(choices=OPTIONS, max_length=100, null=True, blank=True)
     imagen = models.ImageField(null=True, blank=True, upload_to="images/")
     aula = models.CharField(max_length=100, blank=True)
 
