8f26bd02e2c36d868c8ba02f740b17f917eea7c0
Luciano2005 <lumanriquem@unal.edu.co>
2022-11-19 11:07:30 -0500

    Se me olvidó quitarle el modo prueba XD

--- FirstStep/settings.py
+++ FirstStep/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'RENDER' not in os.environ
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
